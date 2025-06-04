2f5c67f4cea97276eee77e314bd49df0aeca433a
LuigiVanin <luisfvaninmartins@gmail.com>
2025-06-04 12:05:15 -0300

    Refactor ♻️: adding debug configuration

--- script.sh
+++ script.sh
@@ -1,12 +1,33 @@
-if [ "$1" = "--build" ]; then
-  cmake -D CMAKE_C_COMPILER=clang . -B ./build
-  
+#!/bin/bash
+
+debug_opt=""
+# Check if --debug is in the arguments
+for arg in "$@"; do
+  if [ "$arg" = "--debug" ]; then
+    debug_opt="-D CMAKE_BUILD_TYPE=Debug"
+    break
+  fi
+done
+
+action="$1"
+if [ "$1" = "--debug" ] && [ -n "$2" ]; then
+  action="$2"
+fi
+
+if [ "$action" = "--build" ]; then
+  echo "Configuring CMake with: -D CMAKE_C_COMPILER=clang ${debug_opt} . -B ./build"
+  cmake -D CMAKE_C_COMPILER=clang ${debug_opt} . -B ./build
   make -C ./build
-elif [ "$1" = "--run" ]; then
+
+elif [ "$action" = "--run" ]; then
   make -C ./build
   ./build/snake_game
+
 else 
-  cmake -D CMAKE_C_COMPILER=clang . -B ./build
-  make -C ./build
+  # Default action: configure, build, and run
+  echo "Configuring CMake with: -D CMAKE_C_COMPILER=clang ${debug_opt} . -B ./build (default action)"
+  cmake -D CMAKE_C_COMPILER=clang ${debug_opt} . -B ./build
+  
+  make -C ./build  
   ./build/snake_game
 fi
